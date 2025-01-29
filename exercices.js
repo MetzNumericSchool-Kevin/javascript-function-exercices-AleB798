b95e23ee7b31a0719f848ba2c2bd4c22909b43cc
Alexandra Baumann <alexbaumann342@gmail.com>
2025-01-29 11:20:44 +0100

    Fabrication de potion

--- exercices.js
+++ exercices.js
@@ -24,48 +24,68 @@ const inventaire = [
 
 // ===== Quel est le tarif d'une potion ? =====
 
-function totalPrice (inventaire, id, quantity=1 ) {
-    let price = 0;
-    inventaire.forEach(potion => {
-        if(id === potion.id) {
-            price = potion.prix*quantity
-        }
-        else {
-            console.log("Nous ne trouvons pas cette potion dans notre inventaire") //Comment mieux gérer les erreurs ?
-        }
-    }); 
-    return price;
+// function totalPrice (inventaire, id, quantity=1 ) {
+//     let price = 0;
+//     inventaire.forEach(potion => {
+//         if(id === potion.id) {
+//             price = potion.prix*quantity
+//         }
+//         else {
+//             console.log("Nous ne trouvons pas cette potion dans notre inventaire") //Comment mieux gérer les erreurs ?
+//         }
+//     }); 
+//     return price;
+// }
+
+// console.log(totalPrice(inventaire, "potion_soin", 3))
+// console.log(totalPrice(inventaire, "potion_so", 3))
+
+
+// // Autres solutions avec boucle for...of  & FIND //
+
+//  function totalPrice (inventaire, id, quantity=1) {
+//     for (let potion of inventaire) {
+//         if(potion.id === id) {
+//             return potion.prix*quantity
+//         }
+//         else {
+//             console.log("Nous ne trouvons pas cette potion dans notre inventaire")
+//         }
+//     }
+//  }
+
+//  console.log(totalPrice(inventaire, "potion_soin", 3))
+//  console.log(totalPrice(inventaire, "potion_courage", 5))
+
+//  function totalPrice(inventaire, id, quantity = 1) {
+//     const potion = inventaire.find(potion => potion.id === id);
+//     if (potion) {
+//         return potion.prix * quantity;
+//     } else {
+//         console.log("Nous ne trouvons pas cette potion dans notre inventaire")
+//     }
+// }
+
+// console.log(totalPrice(inventaire, "potion_soin", 3));
+// console.log(totalPrice(inventaire, "potion_foin", 5));
+
+
+// ===== Fabrication de potion =====
+
+function createPotion (id, prix=10, stock=1) {
+
+    if (typeof id !== "string" || id.trim() === "") {
+        throw new Error("Erreur : l'id de la potion est obligatoire et doit être une chaîne non vide !");
+    } // vérifie que l'on entre un id de type string et que la chaine de caractère n'est pas vide sinon arrête l'éxé et affiche message d'erreur
+    return {
+        id: id,
+        prix: prix,
+        stock: stock
+    }
 }
 
-console.log(totalPrice(inventaire, "potion_soin", 3))
-console.log(totalPrice(inventaire, "potion_so", 3))
+console.log(createPotion("potion_invisibilite", 15, 1));
+console.log(createPotion("potion_bouleFeu", 150));
+console.log(createPotion(10, 2));
 
-
-// Autres solutions avec boucle for...of  & FIND //
-
- function totalPrice (inventaire, id, quantity=1) {
-    for (let potion of inventaire) {
-        if(potion.id === id) {
-            return potion.prix*quantity
-        }
-        else {
-            console.log("Nous ne trouvons pas cette potion dans notre inventaire")
-        }
-    }
- }
-
- console.log(totalPrice(inventaire, "potion_soin", 3))
- console.log(totalPrice(inventaire, "potion_courage", 5))
-
- function totalPrice(inventaire, id, quantity = 1) {
-    const potion = inventaire.find(potion => potion.id === id);
-    if (potion) {
-        return potion.prix * quantity;
-    } else {
-        console.log("Nous ne trouvons pas cette potion dans notre inventaire")
-    }
-}
-
-console.log(totalPrice(inventaire, "potion_soin", 3));
-console.log(totalPrice(inventaire, "potion_foin", 5));
 
