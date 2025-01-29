4d50485583ba9f86c1e21b3d60c4eaac67c3660e
Alexandra Baumann <alexbaumann342@gmail.com>
2025-01-29 09:06:08 +0100

    Salutation Aventurier

---
+++ exercices.js
@@ -0,0 +1,8 @@
+// ===== Salutation Aventurier ===== 
+
+function salutation (nameSorcer) {
+    console.log(`Salutations Aventurier ! Je me nomme ${nameSorcer} pour vous servir..`)
+}
+
+salutation("Archibald")
+
